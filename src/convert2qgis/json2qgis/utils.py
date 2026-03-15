03172f8c2b676784a479fe3e8b4def0155365325
Ivan Ivanov <ivan.ivanov@suricactus.com>
2026-03-15 10:44:10 +0200

    Simplify the ExternalResource widget config definition

--- src/convert2qgis/json2qgis/utils.py
+++ src/convert2qgis/json2qgis/utils.py
@@ -556,20 +556,7 @@ def set_field_widget(field: QgsField, field_def: FieldDef) -> None:
             }
         )
     elif widget_type == "ExternalResource":
-        wc.update(
-            {
-                "DocumentViewer": wc.get("is_document_viewer_enabled", True),
-                "DocumentViewerHeight": wc.get("document_viewer_height", 0),
-                "DocumentViewerWidth": wc.get("document_viewer_width", 0),
-                "FileWidget": wc.get("use_file_widget", True),
-                "FileWidgetButton": wc.get("show_file_widget_button", True),
-                "FileWidgetFilter": wc.get("file_widget_filter", ""),
-                "RelativeStorage": wc.get("use_relative_storage", 1),
-                "StorageAuthConfigId": wc.get("storage_auth_config_id", None),
-                "StorageMode": wc.get("storage_mode", 0),
-                "StorageType": wc.get("storage_type", None),
-            }
-        )
+        wc.update(wc)
     elif widget_type == "ValueMap":
         wc.update(
             {
